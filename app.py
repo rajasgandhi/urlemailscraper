cad803bcd984c83835fed57f7c5dcb8b0df822cc
RajasG <rajasmgandhi@gmail.com>
2020-06-22 18:56:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -92,5 +92,5 @@ def api():
 
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-    app.run()
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+    #app.run()
